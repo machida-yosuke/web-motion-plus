8a049c485ca47b34e60bcc852e8263ffb2a48d71
machida-yosuke <machida-yosuke@kayac.com>
2025-05-13 16:27:45 +0900

    storybook init

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ['../app/**/*.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false,
+      },
+    },
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {
+      builder: {
+        viteConfigPath: 'vite-storybook.config.ts',
+      },
+    },
+  },
+};
+export default config;
